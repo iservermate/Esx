c6bd7f8f99194cfe60a0fec84140bd57947f075b
iServermate <mkazmi0110@gmail.com>
2024-03-09 17:23:20 -0500

    Update CreateVm.ps1

--- roles/Esx/files/CreateVm.ps1
+++ roles/Esx/files/CreateVm.ps1
@@ -1,5 +1,5 @@
 Set-PowerCLIConfiguration -InvalidCertificateAction:ignore
-Connect-VIServer 192.168.0.4 -User root -Password M0hsin!@#
+Connect-VIServer 192.168.0.4 -User root -Password password
 Get-VM
 New-VM -Name icmp-rtest-01 -Datastore Datastore1 -DiskGB 50 -MemoryGB 2 -NumCpu 2 -DiskStorageFormat Thin -CoresPerSocket 1 -CD -VMHost 192.168.0.4 -GuestId centos64Guest
 Get-VM icmp-rtest-01 | Get-CDDrive | Set-CDDrive -ISOPath "[datastore1] OS/centos8.iso" -StartConnected $true -Confirm:$false 1> /dev/null
